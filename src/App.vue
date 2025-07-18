0e95401040dcda8f931df2fbf7e643b202848f26
Kylin <L1ngovo@163.com>
2025-07-18 17:40:35 +0800

    feat: 主页面布局构建

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
     <div class="common-layout">
-        <router-view></router-view>
+        <!-- <router-view></router-view> -->
+        <RouterView />
     </div>
 </template>
 
 <script setup lang="ts">
+import { RouterView } from 'vue-router'
 </script>
 
 <style scoped>
--- src/components/pages/main-page.vue
+++ src/components/pages/main-page.vue
@@ -1,38 +1,42 @@
 <template>
     <div>
-        <!-- 按钮，点击后改变显示状态 -->
-        <button @click="showLoginPage = true">显示滑动页面</button>
+        <div>
+            <Header></Header>
+        </div>
+        <div class="main-page-body">
+            <div>siderbar</div>
+            <div>
+                <div>classify</div>
+                <div>assignment</div>
+            </div>
+            <div>statistic</div>
+        </div>
 
-        <!-- 过渡效果 -->
-        <transition name="slide-down">
-            <!-- 滑动页面，根据 showSlidePage 的值显示或隐藏 -->
-            <LoginPage v-if="showLoginPage" @close="showLoginPage = false" />
-        </transition>
+        <!-- <div>
+            <button @click="showLoginPage = true">显示滑动页面</button>
+
+            <transition name="slide-down">
+                <LoginPage v-if="showLoginPage" @close="showLoginPage = false" />
+            </transition>
+        </div> -->
     </div>
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue'
-import LoginPage from '../auth/login-page.vue' // 引入 SlidePage 组件
+// import { ref } from 'vue'
+import Header from '../common/header-navigation.vue'
+// import LoginPage from '../auth/login-page.vue' // 引入 SlidePage 组件
 
 // 状态管理，控制滑动页面的显示
-const showLoginPage = ref(false)
+// const showLoginPage = ref(false)
 </script>
 
-<style scoped>
-/* 过渡效果的 CSS */
-/* .slide-down-enter-active,
-.slide-down-leave-active {
-    transition: all 0.5s ease;
+<style lang="scss" scoped>
+.main-page-body {
+    display: flex;
+    & > * {
+        flex: 1;
+        text-align: center;
+    }
 }
-.slide-down-enter-from,
-.slide-down-leave-to {
-    transform: translateY(-100%);
-    opacity: 0;
-}
-.slide-down-enter-to,
-.slide-down-leave-from {
-    transform: translateY(0);
-    opacity: 1;
-} */
 </style>
